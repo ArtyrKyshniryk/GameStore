51c54e2cdf9617f338a795a13ad10845e489cff3
ArtyrKyshniryk <k.artyr2005@gmail.com>
2022-05-12 17:37:22 +0300

    Artyr
    Add Models

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Address Address{ get; set; }
+        public List<Product> Products { get; set; }
+        public User User { get; set; }
+        public DateTime SendDate { get; set; }
+        public DateTime DeliverDate { get; set; }
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Models
+{
+    public class User: IdentityUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public List<Review> Reviews { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
